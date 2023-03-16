9dd5cebcc2d787acfbf15ef9acff58637fe47b33
Dominik <dominik.tylczynski@gmail.com>
2023-03-16 16:02:02 +0100

    Configurator class

--- src/pl/sapusers/mfsplc/Configurator.java
+++ src/pl/sapusers/mfsplc/Configurator.java
@@ -3,18 +3,25 @@ package pl.sapusers.mfsplc;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import pl.sapusers.mfsplc.sim.TcpServer;
+import pl.sapusers.mfsplc.sim.TelegramStyle;
 
 public class Configurator {
 	private Logger logger = LogManager.getLogger(Configurator.class.getName());
 	private Properties config;
-	
+	private String configProperties;
+
 	public Configurator(String configProperties, String jcoDestination, String jcoServer) {
+		this.configProperties = configProperties;
 
 		// Load configuration from properties file
 		config = new Properties();
@@ -26,29 +33,68 @@ public class Configurator {
 		} catch (IOException e) {
 			logger.catching(e);
 		}
+		
+		if (jcoDestination != null && !jcoDestination.equals("")) 
+			config.setProperty("jcoDestination", jcoDestination);
+		
+		if (jcoServer != null && !jcoServer.equals("")) 
+			config.setProperty("jcoServer", jcoServer);		
 	}
-	
+
+	private String getProperty(String property) {
+		String value = config.getProperty(property);
+
+		if (value == null || value.equals(""))
+			logger.error(value + " request not defined in the config file: " + configProperties);
+
+		return value;
+	}
+
 	public String getSendingFM() {
-		return config.getProperty("sendingFM");
+		return getProperty("sendingFM");
 	}
-	
+
 	public String getStartingFM() {
-		return config.getProperty("startingFM");
+		return getProperty("startingFM");
 	}
-	
+
 	public String getStoppingFM() {
-		return config.getProperty("stoppingFM");
+		return getProperty("stoppingFM");
 	}
-	
+
 	public String getStatusFM() {
-		return config.getProperty("statusFM");
+		return getProperty("statusFM");
 	}
-	
+
+	public String getJCoDestination() {
+		return getProperty("jcoDestination");
+	}
+
+	public String getJCoServer() {
+		return getProperty("jcoServer");
+	}
+
+	public Boolean getSwitchSenderReceiver() {
+		return Boolean.parseBoolean(getProperty("switchSenderReceiver"));
+	}
+
+	public List<TelegramStyle> getTelegramStyles() {
+		List<TelegramStyle> styles = new ArrayList<TelegramStyle>();
+
+		Set<String> propertyKeys = config.stringPropertyNames();
+
+		for (String propertyKey : propertyKeys) {
+			if (propertyKey.contains("Style")) {
+				styles.add(new TelegramStyle(propertyKey, config.getProperty(propertyKey)));
+			}
+		}
+
+		return styles;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
 	}
 
 }
---
+++ src/pl/sapusers/mfsplc/sim/TelegramStyle.java
@@ -0,0 +1,57 @@
+package pl.sapusers.mfsplc.sim;
+
+import java.awt.Color;
+
+public class TelegramStyle {
+	private String name;
+
+	private Color color;
+
+	private boolean italic = false;
+
+	private boolean bold = false;
+
+	private boolean underline = false;
+
+	private boolean strikeThrough = false;
+
+	public TelegramStyle(String propertyKey, String propertyValue) {
+		name = propertyKey.substring(6);
+
+		String[] styleParts = propertyValue.split(",");
+		color = new Color(Integer.parseInt(styleParts[0]), Integer.parseInt(styleParts[1]),
+				Integer.parseInt(styleParts[2]));
+
+		if (styleParts.length == 4) {
+			if (styleParts[3].contains("I"))
+				italic = true;
+			if (styleParts[3].contains("B"))
+				bold = true;
+			if (styleParts[3].contains("U"))
+				underline = true;
+			if (styleParts[3].contains("S"))
+				strikeThrough = true;
+		}
+
+	}
+	public Color getColor() {
+		return color;
+	}
+	public String getName() {
+		return name;
+	}
+	public boolean isBold() {
+		return bold;
+	}
+	public boolean isItalic() {
+		return italic;
+	}
+	public boolean isStrikeThrough() {
+		return strikeThrough;
+	}
+
+	public boolean isUnderline() {
+		return underline;
+	}
+
+}
