ee7b50c1a0e29555560d0fd75c3a825cc29e7426
Dominik <dominik.tylczynski@gmail.com>
2023-03-16 15:02:26 +0100

    Configurator class

--- src/pl/sapusers/mfsplc/Configurator.java
+++ src/pl/sapusers/mfsplc/Configurator.java
@@ -1,10 +1,22 @@
 package pl.sapusers.mfsplc;
 
-public class Configurator {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import pl.sapusers.mfsplc.sim.TcpServer;
+
+public class Configurator {
+	private Logger logger = LogManager.getLogger(Configurator.class.getName());
+	
+	public Configurator(String jcoDestination, String jcoServer) {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
