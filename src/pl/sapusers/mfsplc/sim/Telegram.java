26f81dbe2e94535bfcb24a6b796906dfedc797e0
Dominik <dominik.tylczynski@gmail.com>
2023-04-09 20:09:00 +0200

    telegram types

--- src/pl/sapusers/mfsplc/sim/Telegram.java
+++ src/pl/sapusers/mfsplc/sim/Telegram.java
@@ -3,45 +3,46 @@ package pl.sapusers.mfsplc.sim;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import com.sap.conn.jco.JCo;
-import com.sap.conn.jco.JCoException;
-import com.sap.conn.jco.JCoRecordMetaData;
-import com.sap.conn.jco.JCoStructure;
-import com.sap.conn.jco.server.JCoServer;
-
-import pl.sapusers.mfsplc.Configurator;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.sap.conn.jco.JCo;
+import com.sap.conn.jco.JCoDestinationManager;
+import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.JCoStructure;
+
+import pl.sapusers.mfsplc.Configurator;
+
 public class Telegram {
 	private Logger logger = LogManager.getLogger(Telegram.class.getName());
-	
+
 	public static final String TO_SAP = "outbound";
-	public static final String FROM_SAP = "inbound";	
+	public static final String FROM_SAP = "inbound";
 
 	private JCoStructure telegramContent;
 	private LocalDateTime timeStamp;
 
-
-	public Telegram(JCoRecordMetaData telegramMetadata, String telegramString) {
-		telegramContent = JCo.createStructure(telegramMetadata);
-		telegramContent.setString(telegramString);
-		
-		timeStamp = LocalDateTime.now();
-	}
-
-	public Telegram(JCoServer server, String telegramStructure, String telegramString) throws JCoException {
-		this(server.getRepository().getStructureDefinition(telegramStructure), telegramString);
-	}
-	
 	public Telegram(JCoStructure telegramContent) {
 		this.telegramContent = telegramContent;
 		timeStamp = LocalDateTime.now();
 	}
-	
+
 	public Telegram(Configurator configurator, String telegramString) {
-		
+		try {
+			JCoStructure header = JCo
+					.createStructure(JCoDestinationManager.getDestination(configurator.getJCoDestination())
+							.getRepository().getStructureDefinition(configurator.getTelegramStructureHeader()));
+			
+			telegramContent = JCo
+					.createStructure(JCoDestinationManager.getDestination(configurator.getJCoDestination())
+							.getRepository().getStructureDefinition(header.getString("TELETYPE")));
+			
+			telegramContent.setString(telegramString);
+			timeStamp = LocalDateTime.now();
+			
+		} catch (IllegalArgumentException | JCoException e) {
+			logger.error(e);
+		}
 	}
 
 	public String getField(String field) {
