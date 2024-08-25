0b7ec6e22ee4a1cc875cf427256b945dbc85f4be
dominik <dominik@dominik-MS-7A15>
2024-08-25 14:54:54 +0200

    Sim2.0

--- src/pl/sapusers/mfsplc/sim/PlcDialog.java
+++ src/pl/sapusers/mfsplc/sim/PlcDialog.java
@@ -1,6 +1,80 @@
 package pl.sapusers.mfsplc.sim;
 
-public class PlcDialog {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JEditorPane;
+
+public class PlcDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			PlcDialog dialog = new PlcDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public PlcDialog() {
+		setBounds(100, 100, 550, 400);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JSplitPane splitPane = new JSplitPane();
+			contentPanel.add(splitPane);
+			{
+				JPanel panel = new JPanel();
+				splitPane.setLeftComponent(panel);
+			}
+			{
+				JPanel panel = new JPanel();
+				splitPane.setRightComponent(panel);
+				panel.setLayout(new BorderLayout(0, 0));
+				{
+					JScrollPane scrollPane = new JScrollPane();
+					scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+					scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+					panel.add(scrollPane, BorderLayout.CENTER);
+				}
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
 
 }
